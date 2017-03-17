e52e40e0c3a2e388c83c7236730f5092446f3d3b
Saumitra Kapoor <saumitrakapoor21@gmail.com>
2017-03-17 21:46:50 +0530

    Update Main.ino

--- NodeMCU/ThingsSpeak/WriteVoltage_EX/Main.ino
+++ NodeMCU/ThingsSpeak/WriteVoltage_EX/Main.ino
@@ -53,13 +53,6 @@
   #define VOLTAGE_MAXCOUNTS 1023.0
 #endif
 
-/*
-  *****************************************************************************************
-  **** Visit https://www.thingspeak.com to sign up for a free account and create
-  **** a channel.  The video tutorial http://community.thingspeak.com/tutorials/thingspeak-channels/ 
-  **** has more information. You need to change this to your channel, and your write API key
-  **** IF YOU SHARE YOUR CODE WITH OTHERS, MAKE SURE YOU REMOVE YOUR WRITE API KEY!!
-  *****************************************************************************************/
 unsigned long myChannelNumber = XXX;
 const char * myWriteAPIKey = "XXX";
 
